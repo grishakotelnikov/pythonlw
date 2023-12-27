1078b7d5d3efcfd395adfbeb7eca603c447c89be
grishakotelnikov <106670234+grishakotelnikov@users.noreply.github.com>
2023-12-27 14:25:12 +0300

    Update puancare.py

--- puancare.py
+++ puancare.py
@@ -2,35 +2,22 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-def lorenz(x, y, z, s=10, r=28, b=2.667):
-    x_dot = s*(y - x)
-    y_dot = r*x - y - x*z
-    z_dot = x*y - b*z
-    return x_dot, y_dot, z_dot
+# Создаем данные для графика
+x = np.linspace(-6, 6, 30)
+y = np.linspace(-6, 6, 30)
 
-dt = 0.01
-step_count = 10000
+X, Y = np.meshgrid(x, y)
+Z = np.sin(np.sqrt(X**2 + Y**2))
 
-xs = np.empty((step_count + 1,))
-ys = np.empty((step_count + 1,))
-zs = np.empty((step_count + 1,))
-
-# Начальные условия
-xs[0], ys[0], zs[0] = (0., 1., 1.05)
-
-# Шаг
-for i in range(step_count):
-    x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
-    xs[i + 1] = xs[i] + (x_dot * dt)
-    ys[i + 1] = ys[i] + (y_dot * dt)
-    zs[i + 1] = zs[i] + (z_dot * dt)
-
+# Создаем трехмерный график
 fig = plt.figure(figsize=(12, 9))
 ax = fig.add_subplot(111, projection='3d')
 
-ax.plot(xs, ys, zs, lw=0.5)
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+
+ax.set_title('3D график')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
 plt.show()
